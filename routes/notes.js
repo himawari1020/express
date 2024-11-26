bad728f52e1954fdad3c775bb3f56db221abadc3
himawari1020 <mizutani.yukihide@gmail.com>
2024-11-26 12:38:31 +0900

    add file

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "";
+const uri = "mongodb+srv://mizutaniyukihide:PIkb3YEBKEDM8Jk6@cluster0.nj8ej.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
 router.get('/', async (req, res) => {
 // データベース、コレクションを指定
